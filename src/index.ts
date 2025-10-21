7f7b5f19e1926251630c7a62c617aa4fd56d8bbc
kpal <kpal81xd@gmail.com>
2025-10-21 15:38:26 +0100

    feat: reorganised rules

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { config as javascript } from './configs/javascript.js';
+import { config as typescript } from './configs/typescript.js';
+
+export default {
+    javascript,
+    typescript,
+};
